ad43bdbc4ed00bd12f8ced446e7f97494ce71776
LandonLiles <98120313+LandonLiles@users.noreply.github.com>
2023-11-26 00:41:40 -0600

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SofEngProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ Models/Leaderboard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SofEngProject.Models
+{
+	public class Leaderboard
+	{
+		public String? Name { get; set; }
+
+        public int Score { get; set; }
+
+
+    }
+}
+
